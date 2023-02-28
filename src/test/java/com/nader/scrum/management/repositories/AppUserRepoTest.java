56a77a4facd14f18077ed9a4e0ac23f9ee9fcf86
NaderAyed <nader.ayed@esprit.tn>
2023-02-28 22:38:46 +0100

    Repo Testing

--- src/test/java/com/nader/scrum/management/repositories/AppUserRepoTest.java
+++ src/test/java/com/nader/scrum/management/repositories/AppUserRepoTest.java
@@ -1,23 +1,30 @@
 package com.nader.scrum.management.repositories;
 
 import com.nader.scrum.management.entities.AppUser;
-import com.nader.scrum.management.entities.Project;
 import com.nader.scrum.management.entities.Role;
-import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-@RequiredArgsConstructor
+
+@DataJpaTest
 class AppUserRepoTest {
 
-    private AppUserRepo appUserRepo;
+  @Autowired
+  AppUserRepo underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
 
     @Test
-    void itShouldCheckIfUserExistsByEmail() {
+    void itShouldCheckWhenUserExistsByEmail() {
 
         /*
             we will add H2 DataBase as inMemory DB just for test, to do that we need to add H2 Dependency
@@ -32,16 +39,45 @@ class AppUserRepoTest {
                 "user100",
                 "last100",
                 Role.DEVELOPER,
-                new ArrayList<Project>(),
-                new ArrayList<Project>()
+                new ArrayList<>(),
+                new ArrayList<>()
+
         );
-        appUserRepo.save(appUser);
+        underTest.save(appUser);
 
         //When
-        boolean exists = appUserRepo.selectExistsEmail("user@email.com");
+        boolean exists = underTest.selectExistsEmail("user@email.com");
 
         //Then
         assertThat(exists).isTrue();
+
+    }
+    @Test
+    void itShouldCheckWhenUserByEmailDoesNotExist() {
+
+        /*
+            we will add H2 DataBase as inMemory DB just for test, to do that we need to add H2 Dependency
+           then create a resources folder under test  finally create app.prop file to configure H2 DB
+         */
+
+        //given
+        AppUser appUser = new AppUser(
+                (long) 100,
+                "user@email.com",
+                "1234",
+                "user100",
+                "last100",
+                Role.DEVELOPER,
+                new ArrayList<>(),
+                new ArrayList<>()
+
+        );
+
+        //When
+        boolean exists = underTest.selectExistsEmail("user@email.com");
+
+        //Then
+        assertThat(exists).isFalse();
 
     }
 }
