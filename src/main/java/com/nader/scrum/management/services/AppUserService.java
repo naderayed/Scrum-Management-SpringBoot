0ccffc46fc18764106b4d5ec7abf63de991472b9
NaderAyed <nader.ayed@esprit.tn>
2023-03-05 20:23:27 +0100

    mocking tests

--- src/main/java/com/nader/scrum/management/services/AppUserService.java
+++ src/main/java/com/nader/scrum/management/services/AppUserService.java
@@ -9,11 +9,10 @@ import com.nader.scrum.management.repositories.AppUserRepo;
 import com.nader.scrum.management.repositories.ProjectRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 
-import java.text.Format;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -27,15 +26,16 @@ public class AppUserService implements IAppUserService, ICrud<AppUser> {
 
     @Override
     public AppUser create(AppUser appUser) {
-        if (appUser != null)
+        Boolean found = appUserRepo.selectExistsEmail(appUser.getEmailUser());
+        if (Boolean.FALSE.equals(found))
             return appUserRepo.save(appUser);
-        return null;
+        throw new NotFoundException("Email is taken, try another Email!");
     }
 
     @Override
     public AppUser get(Long id) {
         return appUserRepo.findById(id)
-                .orElseThrow(() -> new IllegalStateException("No User With ID :" + id));
+                .orElseThrow(() -> new NotFoundException("No User With ID :" + id));
 
     }
 
@@ -63,9 +63,9 @@ public class AppUserService implements IAppUserService, ICrud<AppUser> {
     @Override
     public void assignProjectToDeveloper(int projectId, int devId) {
         Project project = projectRepo.findById((long) projectId)
-                .orElseThrow(() -> new RuntimeException("No Project with ID: " + projectId));
+                .orElseThrow(() -> new NotFoundException("No Project with ID: " + projectId));
         AppUser appUser = appUserRepo.findById((long) devId)
-                .orElseThrow(() -> new RuntimeException("No Developer with ID: " + devId));
+                .orElseThrow(() -> new NotFoundException("No Developer with ID: " + devId));
         if (appUser.getRole() == Role.DEVELOPER) {
             appUser.getDevelopersProjects().add(project);
             appUserRepo.save(appUser);
@@ -78,9 +78,9 @@ public class AppUserService implements IAppUserService, ICrud<AppUser> {
     public void assignProjectToScrumMaster(int projectId, String fName, String lName) {
 
         AppUser appUser = appUserRepo.findByFirstnameAndLastname(fName, lName)
-                .orElseThrow(() -> new RuntimeException("No User found named " + fName + " " + lName));
+                .orElseThrow(() -> new NotFoundException("No User found named " + fName + " " + lName));
         Project project = projectRepo.findById((long) projectId)
-                .orElseThrow(() -> new RuntimeException("No Project found With Id " + projectId));
+                .orElseThrow(() -> new NotFoundException("No Project found With Id " + projectId));
         appUser.getScrumProjects().add(project);
         appUserRepo.save(appUser);
     }
@@ -88,7 +88,7 @@ public class AppUserService implements IAppUserService, ICrud<AppUser> {
     @Override
     public AppUserDTO findUserByEmail(String email) {
         AppUser appUser = appUserRepo.findAppUserByEmailUser(email)
-                .orElseThrow(() -> new RuntimeException("No User With Email : " + email));
+                .orElseThrow(() -> new NotFoundException("No User With Email : " + email));
 
         return new AppUserDTO(appUser.getIdUser(),
                 appUser.getEmailUser(),
--- src/test/java/com/nader/scrum/management/services/AppUserServiceTest.java
+++ src/test/java/com/nader/scrum/management/services/AppUserServiceTest.java
@@ -1,15 +1,24 @@
 package com.nader.scrum.management.services;
 
 import com.nader.scrum.management.dto.AppUserDTOMapper;
+import com.nader.scrum.management.entities.AppUser;
+import com.nader.scrum.management.entities.Role;
 import com.nader.scrum.management.repositories.AppUserRepo;
 import com.nader.scrum.management.repositories.ProjectRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.webjars.NotFoundException;
 
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,17 +27,17 @@ class AppUserServiceTest {
 
     @Mock
     private AppUserRepo appUserRepo;
- //   private AutoCloseable autoCloseable;
+    //   private ll autoCloseable;
     @Mock
     private ProjectRepo projectRepo;
     @Mock
-    private AppUserDTOMapper appUserDTOMapper ;
+    private AppUserDTOMapper appUserDTOMapper;
 
     private AppUserService underTest;
 
     @BeforeEach
     void setUp() {
-     //   autoCloseable = MockitoAnnotations.openMocks(this);
+        //   autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new AppUserService(appUserRepo, projectRepo, appUserDTOMapper);
     }
 
@@ -49,6 +58,55 @@ class AppUserServiceTest {
     @Test
     @Disabled
     void update() {
+    }
+
+    @Test
+    void canAddAppUser() {
+        //given
+        AppUser appUser = new AppUser(
+                (long) 100,
+                "email@email.com",
+                "1234",
+                "user100",
+                "last100",
+                Role.DEVELOPER,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        //when
+        underTest.create(appUser);
+
+        //then
+
+        ArgumentCaptor<AppUser> appUserArgumentCaptor = ArgumentCaptor.forClass(AppUser.class);
+
+        verify(appUserRepo).save(appUserArgumentCaptor.capture());
+        AppUser captorUser = appUserArgumentCaptor.getValue();
+        assertThat(captorUser).isEqualTo(appUser);
+    }
+
+    @Test
+    void willThrowWhenEmailGiven() {
+        //given
+        AppUser appUser = new AppUser(
+                (long) 100,
+                "email@email.com",
+                "1234",
+                "user100",
+                "last100",
+                Role.DEVELOPER,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+        given(appUserRepo.selectExistsEmail(appUser.getEmailUser())).willReturn(true);
+
+        //when
+        //then
+
+            assertThatThrownBy(() -> underTest.create(appUser))
+                    .isInstanceOf(NotFoundException.class)
+                    .hasMessageContaining("Email is taken, try another Email!");
     }
 
     @Test
